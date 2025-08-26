7afbbf151f9a2c53b398a757f8751ad330f2b8ba
Nghia-opp <nt14499@gmail.com>
2025-08-27 00:07:30 +0900

    session12_exercise01_bai02

---
+++ bai02/bai02.js
@@ -0,0 +1,7 @@
+let age = prompt("Please input your age");
+
+if (age >= 16){
+    alert("Bạn đủ điều kiện vào học lớp 10 của trường THPT");
+} else {
+    alert("Bạn không đủ điều kiện vào học lớp 10");
+}
