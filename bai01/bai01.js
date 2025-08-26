3f9edf364489a301981d2b60b70e75bfe866d22b
Nghia-opp <nt14499@gmail.com>
2025-08-27 00:07:14 +0900

    session12_exercise01_bai01

---
+++ bai01/bai01.js
@@ -0,0 +1,9 @@
+let a = +prompt("Please iuput your number a :");
+let b = +prompt("Please iuput your number b :");
+
+let number = a % b;
+if (number === true) {
+        console.log("Congratulations");
+} else {
+    console.log("false");
+}
