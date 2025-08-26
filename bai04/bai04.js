8b921ba4cca43c6309b63830d2d162427c49f7c2
Nghia-opp <nt14499@gmail.com>
2025-08-27 00:07:56 +0900

    session12_exercise01_bai04

---
+++ bai04/bai04.js
@@ -0,0 +1,12 @@
+const a = +prompt("Mời bạn nhập số a");
+const b = +prompt("Mời bạn nhập số b");
+const c = +prompt("Mời bạn nhập số c");
+
+if (a>=b && a>=b){
+    console.log(`Giá trị lớn nhất là ${a}` );        
+} else if(b>=a && b>=c) {
+    console.log(`Giá trị lớn nhất là ${b}` ); 
+} else {
+    console.log(`Giá trị lớn nhất là ${c}` ); 
+}
+
